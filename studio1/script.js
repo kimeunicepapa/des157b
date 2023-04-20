ea144aea470a9fe18773d866888a05fd27dd62a0
Kim Eunice Papa <papakimeunice@gmail.com>
2023-04-20 13:48:07 -0700

    done

--- studio1/script.js
+++ studio1/script.js
@@ -6,23 +6,23 @@
     const myQuote = document.querySelector('#quote');
     const fs = document.querySelector(".fa-expand");
 
-
-
     let prevXLoc = 0;
     let prevXLocBack = 0;
     
     function reportPos (event){
         const windowWidth = window.innerWidth;
         const widthPercent = windowWidth / 181;
+        const widthPercentScale = windowWidth / 2;
 
         console.log(`width percent ${widthPercent}`);
         const xPos = event.clientX;
         const changeRotation = Math.floor(xPos/ widthPercent);
+        const changeScale = xPos/widthPercentScale;
 
         console.log(`change rotation is ${changeRotation}`);
 
         if(changeRotation !== prevXLoc){
-            myVideo.style.transform = `rotate(${changeRotation}deg)`;
+            myVideo.style.transform = `rotate(${changeRotation}deg) scale(${changeScale})`;
             prevXLoc = changeRotation;
         }
 
