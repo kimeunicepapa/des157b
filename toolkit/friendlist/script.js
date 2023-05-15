20ec275dc29e3472e79fa435aa0cbeb141aebd39
Kim Eunice Papa <papakimeunice@gmail.com>
2023-05-15 02:46:58 -0700

    backapp5

--- toolkit/friendlist/script.js
+++ toolkit/friendlist/script.js
@@ -10,6 +10,7 @@
     const editFriendForm = document.getElementById("edit-friend");
     const friendList = document.querySelector("main ol");
     const inputs = document.querySelectorAll("#add-friend input:not([type=submit])");
+    let thisRecord;
 
     async function displayFriends() {
         const friends = Parse.Object.extend('Friends');
@@ -103,17 +104,84 @@
         }
     }
 
-    for(let i=0; i<editBtns.length; i++){
-        editBtns[i].addEventListener("click", function(event){
-            event.preventDefault();
-            editFriendForm.className = "edit-friend-onscreen";
-        })
-    }
+    // for(let i=0; i<editBtns.length; i++){
+    //     editBtns[i].addEventListener("click", function(event){
+    //         event.preventDefault();
+    //         editFriendForm.className = "edit-friend-onscreen";
+    //     })
+    // }
+
+    document.addEventListener('click', function(event){
+
+        if (event.target.matches('.fa-edit')) {
+            thisRecord = event.target.getAttribute('id').slice(2);
+            setForm(thisRecord);
+        }
+        
+    }, false);
 
     editFriendForm.addEventListener('submit', function(event){
         event.preventDefault();
-        editFriendForm.className = "edit-friend-offscreen";
-    })
+        // editFriendForm.className = "edit-friend-offscreen";
+        updateRecord(thisRecord);
+    });
+
+    async function setForm (recordId) {
+        const friends = Parse.Object.extend('Friends');
+        const query = new Parse.Query(friends);
+        query.equalTo("objectId", recordId);
+
+        try{
+            const results = await query.find();
+            results.forEach( function (thisFriend) {
+                document.getElementById("fname-edit").value = thisFriend.get('fname');
+                document.getElementById("lname-edit").value = thisFriend.get('lname');
+                document.getElementById("email-edit").value = thisFriend.get('email');
+                document.getElementById("fbook-edit").value = thisFriend.get('facebook');
+                document.getElementById("twitter-edit").value = thisFriend.get('twitter');
+                document.getElementById("insta-edit").value = thisFriend.get('instagram');
+                document.getElementById("linkedin-edit").value = thisFriend.get('linkedin');
+            });
+            editFriendForm.className = "edit-friend-onscreen";
+        } catch (error) {
+            console.error('Error while fetching Friends', error);
+        }
+    }
+
+    async function updateRecord(recordId){
+        const theFields = document.querySelectorAll("#edit-friend input:not([type=submit])");
+        const editedRecord = {};
+        let key;
+        let value;
+        for(let i=0; i<theFields.length; i++){
+            key = theFields[i].getAttribute("name");
+            value = theFields[i].value;
+            editedRecord[key] = value;
+        }
+
+        const friends = Parse.Object.extend('Friends');
+        const query = new Parse.Query(friends);
+        try {
+            const object = await query.get(recordId);
+            object.set('fname', editedRecord.fname);
+            object.set('lname', editedRecord.lname);
+            object.set('email', editedRecord.email);
+            object.set('facebook', editedRecord.facebook);
+            object.set('twitter', editedRecord.twitter);
+            object.set('instagram', editedRecord.instagram);
+            object.set('linkedin', editedRecord.linkedin);
+            try {
+                await object.save();
+                editFriendForm.className = "edit-friend-offscreen";
+                friendList.innerHTML = '';
+                displayFriends();
+            } catch (error) {
+                console.error('Error while updating Friends', error);
+            }
+        } catch (error) {
+            console.error('Error while retriving object Friends', error);
+        }
+    }
 
     function resetFormFields(){
         document.getElementById("fname").value = "";
